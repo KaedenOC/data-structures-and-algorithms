33ea2578b8d519b127ed3c77fcdafe0c853dfc3f
KaedenOC <komeacarr2@gmail.com>
2023-05-23 16:15:06 -0700

    work on tests

--- javascript/linked-list/index.js
+++ javascript/linked-list/index.js
@@ -60,6 +60,29 @@ class LinkedList {
     result += 'NULL';
     return result;
   }
+  kthFromEnd(k) {
+    if (k < 0 || !Number.isInteger(k) || this.head === null) {
+      return null;
+    }
+
+    let slow = this.head;
+    let fast = this.head;
+
+    for (let i = 0; i <= k; i++) {
+      if (fast === null) {
+        return null;
+      }
+      fast = fast.next;
+    }
+
+    while (fast !== null) {
+      slow = slow.next;
+      fast = fast.next;
+    }
+
+    return slow.value;
+  }
+
 }
 
 
