644b6f103c116a85b9aba0972a74ebcd7a3e7ecb
KaedenOC <komeacarr2@gmail.com>
2023-05-27 17:28:33 -0700

    successfully testing 4 tests for stack

--- javascript/code-challenges/stacks-and-queues/__tests__/stack.test.js
+++ javascript/code-challenges/stacks-and-queues/__tests__/stack.test.js
@@ -1,17 +1,17 @@
 'use strict';
 
-const { Stack } = require('./index');
+const { Stack } = require('../index');
 
 describe('Stack', () => {
-  test('can successfully push onto a stack', () => {
+  test('Can successfully push onto a stack', () => {
     let stack = new Stack();
-    stack.push();
+    stack.push(1);
 
     expect(stack.top.value).toEqual(1);
     expect(stack.top.next).toBeNull();
   });
 
-  test('can successfully push multiple values onto a stack', () => {
+  test('Can successfully push multiple values onto a stack', () => {
     let stack = new Stack();
     stack.push(1);
     stack.push(2);
@@ -22,5 +22,41 @@ describe('Stack', () => {
     expect(stack.top.next.next.value).toEqual(1);
     expect(stack.top.next.next.next).toBeNull();
   });
+
+  test('Can successfully pop off the stack', () => {
+    let stack = new Stack();
+    stack.push(1);
+    stack.push(2);
+
+    expect(stack.top.value).toEqual(2);
+    expect(stack.top.next.value).toEqual(1);
+    expect(stack.top.next.next).toBeNull();
+
+    stack.pop();
+
+    expect(stack.top.value).toEqual(1);
+    expect(stack.top.next).toBeNull();
+
+  });
+
+  test('Can successfully empty a stack after multiple pops', () => {
+    let stack = new Stack();
+    stack.push(1);
+    stack.push(2);
+
+    expect(stack.top.value).toEqual(2);
+    expect(stack.top.next.value).toEqual(1);
+    expect(stack.top.next.next).toBeNull();
+
+    stack.pop();
+
+    expect(stack.top.value).toEqual(1);
+    expect(stack.top.next).toBeNull();
+    stack.pop();
+    expect(stack.top).toBeNull();
+
+  });
+
+  
 
 });
--- javascript/code-challenges/stacks-and-queues/index.js
+++ javascript/code-challenges/stacks-and-queues/index.js
@@ -1,3 +1,34 @@
 'use strict';
 
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
 
+class Stack {
+  constructor() {
+    this.top = null;
+  }
+  push(value) {
+    let newNode = new Node (value);
+    newNode.next = this.top;
+    this.top = newNode;
+  }
+
+  pop() {
+    let removedValue = null;
+    if (this.top) {
+      removedValue = this.top.value;
+      this.top = this.top.next;
+    }
+    return removedValue;
+  }
+
+  peek() {
+    return this.top.value;
+  }
+}
+
+module.exports = { Stack };
