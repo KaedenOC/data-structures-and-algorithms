98c7affad55b532e8c1297e083b9b2910bf76b09
KaedenOC <komeacarr2@gmail.com>
2023-06-28 15:11:56 -0700

    last commit message incorrect

--- javascript/code-challenges/treeIntersection/tree-intersection.js
+++ javascript/code-challenges/treeIntersection/tree-intersection.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const { HashTable } = require('../hash-table/index');
+const { Tree } =  require('../trees/index');
 
 function tree_intersection(tree1, tree2) {
   let hashTable = new HashTable(100);
@@ -51,5 +52,30 @@ function repeatedWords(str) {
 
   return repeatedWord;
 }
+
+const tree1 = new Tree();
+tree1.root = new Node('150');
+tree1.root.left = new Node('100');
+tree1.root.right = new Node('250');
+tree1.root.left.left = new Node('75');
+tree1.root.left.right = new Node('160');
+tree1.root.right.left = new Node('200');
+tree1.root.right.right = new Node('350');
+tree1.root.left.right.left = new Node('125');
+tree1.root.left.right.right = new Node('175');
+tree1.root.right.right.left = new Node('300');
+tree1.root.right.right.right = new Node('500');
+const tree2 = new Tree();
+tree2.root = new Node('42');
+tree2.root.left = new Node('100');
+tree2.root.right = new Node('600');
+tree2.root.left.left = new Node('15');
+tree2.root.left.right = new Node('160');
+tree2.root.right.left = new Node('200');
+tree2.root.right.right = new Node('350');
+tree2.root.left.right.left = new Node('125');
+tree2.root.left.right.right = new Node('175');
+tree2.root.right.right.left = new Node('4');
+tree2.root.right.right.right = new Node('500');
 
 module.exports = { HashTable, repeatedWords, tree_intersection, };
